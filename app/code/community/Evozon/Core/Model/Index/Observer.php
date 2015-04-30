61d07e6e87349dba42f8eb7437d0382c0560b517
Constantin  Bejenaru <constantin.bejenaru@evozon.com>
2015-04-30 12:18:31 +0300

    Add indexer event 'delete' on 'catalog_category' entity

---
+++ app/code/community/Evozon/Core/Model/Index/Observer.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Observer model for Index module
+ *
+ * @package     Evozon_Core_Model_Index
+ * @copyright   Copyright (c) Evozon Systems (http://www.evozon.com)
+ * @author      Constantin Bejenaru <constantin.bejenaru@evozon.com>
+ */
+class Evozon_Core_Model_Index_Observer
+{
+
+    /**
+     * Register delete event on catalog category entity
+     *
+     * @event catalog_category_delete_before
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function logCatalogCategoryEventDelete(Varien_Event_Observer $observer)
+    {
+        Mage::getSingleton('index/indexer')->logEvent(
+            $observer->getEvent()->getDataObject(),
+            Mage_Catalog_Model_Category::ENTITY,
+            Mage_Index_Model_Event::TYPE_DELETE
+        );
+    }
+
+    /**
+     * Index delete event on catalog category entity
+     *
+     * @event catalog_category_delete_commit_after
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function indexCatalogCategoryEventDelete(Varien_Event_Observer $observer)
+    {
+        Mage::getSingleton('index/indexer')->indexEvents(
+            Mage_Catalog_Model_Category::ENTITY, Mage_Index_Model_Event::TYPE_DELETE
+        );
+    }
+}
